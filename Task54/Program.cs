57104b53dd341ad338649a483fa8ae1dee9bde0d
Beslan06 <bor.mm2011@yandex.ru>
2022-08-31 15:41:52 +0300

    8 Семинар

---
+++ Task54/Program.cs
@@ -0,0 +1,73 @@
+﻿//Задача 54: Задайте двумерный массив. Напишите программу, 
+//которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+Console.Clear();
+Console.WriteLine($"\nВведите размер массива m x n и диапазон случайных значений:");
+int row = InputNumbers("Введите row: ");
+int col = InputNumbers("Введите col: ");
+int range = InputNumbers("Введите диапазон: от 1 до ");
+
+int[,] matrix = new int[row, col];
+CreateArray(matrix);
+WriteArray(matrix);
+
+Console.WriteLine($"\nОтсортированный массив: ");
+OrderArrayLines(matrix);
+WriteArray(matrix);
+
+void OrderArrayLines(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+      {
+        if (matrix[i, k] < matrix[i, k + 1])
+        {
+          int temp = matrix[i, k + 1];
+          matrix[i, k + 1] = matrix[i, k];
+          matrix[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void CreateArray(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      matrix[i, j] = new Random().Next(range);
+    }
+  }
+}
+
+void WriteArray(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      Console.Write($"{matrix[i, j],3}" + " ");
+    }
+    Console.WriteLine();
+  }
+}
